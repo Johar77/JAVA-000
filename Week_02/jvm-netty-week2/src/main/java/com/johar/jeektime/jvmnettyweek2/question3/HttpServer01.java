cb8829868d736c229504f149c194899c3b6c01d7
Johar77 <Johar77@163.com>
2020-10-31 11:04:04 +0800

    提交：HttpServer01请求结果可以在浏览器显示

---
+++ Week_02/jvm-netty-week2/src/main/java/com/johar/jeektime/jvmnettyweek2/question3/HttpServer01.java
@@ -0,0 +1,53 @@
+package com.johar.jeektime.jvmnettyweek2.question3;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @ClassName: HttpServer01
+ * @Description: TODO
+ * @Author: Johar
+ * @Date: 2020/10/31 10:59
+ * @Since: 1.0.0
+ */
+@Slf4j
+public class HttpServer01 {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8801);
+        while (true){
+            try{
+                Socket socket = serverSocket.accept();
+                service(socket);
+            } catch (IOException e){
+                log.error("Accept socket error: ", e);
+            }
+        }
+    }
+
+    private static void service(Socket socket) {
+        try{
+            Thread.sleep(20);
+            try (PrintWriter printWriter = new PrintWriter(socket.getOutputStream())) {
+                printWriter.println("HTTP/1.1 200 OK");
+                printWriter.println("Content-Type:text/html;charset=utf-8");
+                String body = "hello, nio";
+
+                // 增加Content-Length，方便请求客户端通过body的长度解析body内容
+                printWriter.println("Content-Length:" + body.getBytes("UTF-8").length);
+                // socket连接结束，短连接
+                printWriter.println("Connection:close");
+
+                printWriter.println();
+                printWriter.write(body);
+            } catch (IOException e){
+                log.error("getOutPutStream error: ", e);
+            }
+        } catch (InterruptedException e) {
+            log.error("Thread sleep error: ", e);
+        }
+    }
+}
