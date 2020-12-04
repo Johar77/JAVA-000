c271525db4ffbed6bb64e5052f10ce39bdc656c0
Johar77 <Johar77@163.com>
2020-12-04 08:59:35 +0800

    [ADD]提交multi-data-sources,shardingsphere-jdbc

---
+++ Week_07/shardingshere-jdbc/src/main/java/com/johar/geektime/shardingsherejdbc/ShardingshereJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.johar.geektime.shardingsherejdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingshereJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingshereJdbcApplication.class, args);
+    }
+
+}
