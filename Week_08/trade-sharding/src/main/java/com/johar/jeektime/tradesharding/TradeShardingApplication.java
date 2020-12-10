d3f7597f515c457701b5d503238c1aa42776f29c
Johar77 <Johar77@163.com>
2020-12-10 08:36:07 +0800

    [ADD]trade-sharding项目创建

---
+++ Week_08/trade-sharding/src/main/java/com/johar/jeektime/tradesharding/TradeShardingApplication.java
@@ -0,0 +1,13 @@
+package com.johar.jeektime.tradesharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeShardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeShardingApplication.class, args);
+    }
+
+}
